5c2648c15247f4a3669d5996fc084aa7fbe5f838
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2023-02-17 16:57:34 +0000

    ESM and homogenise CID's.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { CID } from "multiformats/cid";
 import { default as crypto } from "crypto";
 
 import { Client } from "@opensearch-project/opensearch";
-import { default as esb, script } from "elastic-builder";
+import { default as esb } from "elastic-builder";
 
+const scrollTime = "1m";
 const year = 2019;
+const index = "ipfs_files";
+const batchSize = 100;
+
 const requestBody = new esb.RequestBodySearch()
 	.query(
 		new esb.BoolQuery()
@@ -15,7 +20,7 @@ const requestBody = new esb.RequestBodySearch()
 			)
 			.filter(new esb.ExistsQuery("references"))
 	)
-	.size(10)
+	.size(batchSize)
 	.sort(new esb.Sort("_doc"))
 	.source("references");
 
@@ -25,9 +30,9 @@ const client = new Client({
 
 async function* getDocuments(client: Client, year: Number) {
 	var response = await client.search({
-		index: "ipfs_files",
+		index: index,
 		body: requestBody.toJSON(),
-		scroll: "1m",
+		scroll: scrollTime,
 	});
 
 	console.info(`Query returned ${response.body.hits.total.value} results.`);
@@ -39,7 +44,7 @@ async function* getDocuments(client: Client, year: Number) {
 	while (response.body.hits.hits.length > 0) {
 		response = await client.scroll({
 			body: {
-				scroll: "1m",
+				scroll: scrollTime,
 				scroll_id: response.body._scroll_id,
 			},
 		});
@@ -60,12 +65,16 @@ function hashId(input: string): string {
 	);
 }
 
+function homogeniseCID(cid: string): string {
+	return CID.parse(cid).toV1().toString();
+}
+
 async function* getLinks(documents: AsyncGenerator) {
 	for await (const doc of getDocuments(client, year)) {
-		const to = doc._id;
+		const to = homogeniseCID(doc._id);
 
 		for (const ref of doc._source.references) {
-			const from = ref.parent_hash,
+			const from = homogeniseCID(ref.parent_hash),
 				name = ref.name;
 
 			yield {
